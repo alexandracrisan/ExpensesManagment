160ddb0003c84481824a87d4334739aa5bdc0c5b
Alexandra <alexandra.crisan@evozon.com>
2015-11-04 11:06:51 +0200

    ReactDOM

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var App = require('./components/app.js');
 
+var Router = require('react-router'),
+    DefaultRoute = Router.DefaultRoute,
+    Link = Router.Link,
+    Route = Router.Route,
+    RouteHandler = Router.RouteHandler;
+
 //renders the whole app
-React.render(<App />, document.getElementById('expensesApp'));
+ReactDOM.render(<App />, document.getElementById('expensesApp'));
 
