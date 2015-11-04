50a339a6378e03a1d6fa3fa197cf81e62ee7a8ca
alina <alina.oros@evozon.com>
2015-11-04 17:07:26 +0200

    create dashboard constants

---
+++ src/js/constants/dashboard-constants.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alinaoros on 11/4/2015.
+ */
+
+var DashboardConstants = {
+    GET_CURRENT_MONTH_BUDGET: "GET_CURRENT_MONTH_BUDGET",
+    GET_CURRENT_MONTH_EXPENSES: "GET_CURRENT_MONTH_EXPENSES",
+    GET_CURRENT_MONTH_LEFT_BUDGET: "GET_CURRENT_MONTH_LEFT_BUDGET"
+};
+
+module.exports = DashboardConstants;
