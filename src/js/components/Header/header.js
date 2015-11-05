61e753fb5770b81cde879c00868062afd09c4f76
Mihaigit config --global user.name Mihaigit config --global user.name Mihai <mihai.gordan@evozon.com>
2015-11-05 17:06:45 +0000

    stylized header and navbar

--- src/js/components/Header/header.js
+++ src/js/components/Header/header.js
@@ -7,13 +7,17 @@ var Link = require('react-router').Link;
 var Header = React.createClass({
     render: function(){
         return (
-            <div>
-                <Link to="addCategory">Add Category</Link>
-                <Link to="addIncome">Add Income</Link>
-                <Link to="addExpenses">Add Expenses</Link>
+            <div className="header-component ">
+				<ul className="nav nav-pills right ">
+				  <li role="presentation" ><Link to="income">Add Income</Link></li>
+				  <li role="presentation"><Link to="income">Add Income</Link></li>
+				  <li role="presentation"> <Link to="income">Add Income</Link></li>
+				</ul>
             </div>
         );
     }
 });
 
 module.exports = Header;
+
+
--- src/js/components/Income/addIncome.js
+++ src/js/components/Income/addIncome.js
@@ -1,5 +1,6 @@
 var React = require('react');
-
+var SidebarBox = require('../../components/Sidebar/sidebarBox.js'),
+	Header = require('../../components/Header/header.js');
 var Bootstrap = require('react-bootstrap');
 var Input = Bootstrap.Input;
 var ButtonInput  = Bootstrap.ButtonInput;
@@ -32,14 +33,24 @@ var AddIncome = React.createClass({
 
 	render: function(){
 		return(
-			<div className="container row">
-				<div className="col-md-4 col-md-offset-4">
-					<form >
-						<Input type="number" className="suma" label="Suma" ref="suma" defaultValue={this.state.suma} placeholder="Enter amount" />
-						<Input type="text" className="dataTime" label="Data" ref="dataTime" defaultValue={this.state.dataTime} placeholder="Enter Data" />
-						<ButtonInput className="btn btn-default" type="submit" value="Add income" onClick={this.addIncome} />
-					</form>
+			<div>
+				<Header />
+			<div>
+				<div className="row">
+					<div>
+					<SidebarBox />
+					</div>
+					<div className="container row ">
+						<div className="col-md-4 col-md-offset-4 income-contente">
+							<form >
+								<Input type="number" className="suma" label="Suma" ref="suma" defaultValue={this.state.suma} placeholder="Enter amount" />
+								<Input type="text" className="dataTime" label="Data" ref="dataTime" defaultValue={this.state.dataTime} placeholder="Enter Data" />
+								<ButtonInput className="btn btn-default" type="submit" value="Add income" onClick={this.addIncome} />
+							</form>
+						</div>
+					</div>
 				</div>
+			</div>
 			</div>
 			);
 	}
