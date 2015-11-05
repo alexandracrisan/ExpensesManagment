d232a5d358d49f5d9351de4fe7e4f61f46aeaca2
alina <alina.oros@evozon.com>
2015-11-05 15:15:28 +0200

    update dashboard store and details page

--- src/js/components/Dashboard/details.js
+++ src/js/components/Dashboard/details.js
@@ -6,63 +6,45 @@ var DashboardStore = require('../../stores/dashboard-store.js');
 var React = require('react');
 console.log('details.js');
 
-//function BudgetDetails() {
-//    return {
-//        budget: DashboardStore.getCurrentMonthBudget(),
-//        expenses: DashboardStore.getCurrentMonthExpenses(),
-//        left: DashboardStore.getCurrentMonthLeftBudget()
-//    }
-//}
-//
-//var BudgetDetailsBoard = React.createClass({
-//    getInitialState: function() {
-//        return BudgetDetails();
-//    },
-//    componentWillMount: function() {
-//        DashboardStore.addChangeListener(this._onChange());
-//    },
-//    componentWillUnmount: function() {
-//        DashboardStore.removeChangeListener()(this._onChange());
-//    },
-//    _onChange: function() {
-//        this.setState(BudgetDetails());
-//    },
-//    render: function() {
-//        return (
-//            <div>
-//                <label>Income: </label>
-//                <label>{this.state.budget}</label>
-//                <br/>
-//                <label>Expenses: </label>
-//                <label>{this.state.expenses}</label>
-//                <br/>
-//                <label>Left: </label>
-//                <label>{this.state.left}</label>
-//
-//            </div>
-//        )
-//    }
-//
-//});
+function BudgetDetails() {
+    return {
+        budget: DashboardStore.getCurrentMonthBudget(),
+        expenses: DashboardStore.getCurrentMonthExpenses(),
+        left: DashboardStore.getCurrentMonthLeftBudget()
+    }
+}
 
 var BudgetDetailsBoard = React.createClass({
+    getInitialState: function() {
+        return BudgetDetails();
+    },
 
     render: function() {
         return (
             <div>
                 <label>Income: </label>
-                <label>300</label>
+              <label>{this.state.budget}</label>
                 <br/>
                 <label>Expenses: </label>
-                <label>30</label>
+                <label>{this.state.expenses}</label>
                 <br/>
                 <label>Left: </label>
-                <label>40</label>
+                <label>{this.state.left}</label>
 
             </div>
         )
     }
 
 });
+
+//componentWillMount: function() {
+//    DashboardStore.addChangeListener(this._onChange());
+//},
+//componentWillUnmount: function() {
+//    DashboardStore.removeChangeListener(this._onChange());
+//},
+//_onChange: function() {
+//    this.setState(BudgetDetails());
+//},
 
 module.exports = BudgetDetailsBoard;
