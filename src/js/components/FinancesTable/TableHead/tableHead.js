ce33e3f44a01595584d10e8b2969232ef884543a
Diana Plesa <diana.plesa@evozon.com>
2015-11-16 15:27:29 +0200

    added table header for finances

--- src/js/components/FinancesTable/TableHead/tableHead.js
+++ src/js/components/FinancesTable/TableHead/tableHead.js
@@ -4,7 +4,24 @@ var TableHead = React.createClass({
 
 	render: function(){
 		return (
-			
+			<table>
+				<thead>
+					<tr>
+						<th>Nr. crt</th>
+						<th>Date</th>
+						<th>Desciption</th>
+						<th>Category</th>
+						<th>Debit</th>
+						<th>Credit</th>
+						</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<td>Sold</td>
+						<td>$100</td>
+					</tr>
+				</tfoot>
+			</table>
 		);
 	}
 });
