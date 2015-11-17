a580bc13d66401f6aec6b192f0d4f2e5df6083d8
Diana Plesa <diana.plesa@evozon.com>
2015-11-17 18:11:26 +0200

    added center style for the text from table fields

--- src/js/components/FinancesTable/TableHead/tableHead.js
+++ src/js/components/FinancesTable/TableHead/tableHead.js
@@ -8,12 +8,12 @@ var TableHead = React.createClass({
 				<form action='#' method='post'>
 					<thead>
 						<tr>
-							<th align="center">Nr. crt</th>
-							<th align="center">Date</th>
-							<th align="center">Desciption</th>
-							<th align="center">Category</th>
-							<th align="center">Debit</th>
-							<th align="center">Credit</th>
+							<th>Nr. crt</th>
+							<th>Date</th>
+							<th>Desciption</th>
+							<th>Category</th>
+							<th>Debit</th>
+							<th>Credit</th>
 							<th>
 								<button className="btn btn-default btn-sm all-space">
 									<i className="glyphicon glyphicon-trash"></i> <strong>Delete</strong>
