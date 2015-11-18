24b9e02294e6d101f8cd1208ddba0ee81e712161
alina <alina.oros@evozon.com>
2015-11-18 10:35:04 +0200

    create modal content in a different folder

--- src/js/actions/category-actions.js
+++ src/js/actions/category-actions.js
@@ -1,3 +1,21 @@
 /**
  * Created by alinaoros on 11/16/2015.
  */
+
+ var CategoryConstants = require('../constants/category-constants.js');
+ var Dispatcher = require('../dispatchers/app.dispatcher.js');
+
+ var ActionTypes = CategoryConstants.ActionTypes;
+
+ var CaregoryAction = {
+
+ 	addCategory: function(data) {
+
+ 		Dispatcher.dispatch({
+ 			type: ActionTypes.ADD_CATEGORY,
+ 			data: data
+ 		});
+ 	}
+ };
+
+ module.exports = CaregoryAction;
