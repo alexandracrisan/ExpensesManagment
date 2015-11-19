a026b9e7d9683d03ae21f85326cd59dfa34959e5
Mihaigit config --global user.name Mihaigit config --global user.name Mihai <mihai.gordan@evozon.com>
2015-11-19 08:56:17 +0000

    nothing to commit

--- src/js/components/Balance/balance.js
+++ src/js/components/Balance/balance.js
@@ -1,25 +1,14 @@
 var React = require('react');
 var SidebarBox = require('../../components/Sidebar/sidebarBox.js'),
-	Header = require('../../components/Header/header.js');
+	Header = require('../../components/Header/header.js'),
+	Table = require('../Balance/Table/table.js');
 
 var Balance = React.createClass({
 
 	render: function(){
 		return (
 			<div>
-				<Header />
-				<div className="row">
-					<SidebarBox />
-					<div id="page-content-wrapper">
-		            	<div className="container-fluid">
-		                	<div className="row">
-		                    	<div className="col-lg-12">
-									<p>Balances</p>		
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<Table/>
 			</div>
 		);
 	}
--- src/js/components/Categories/addCategory.js
+++ src/js/components/Categories/addCategory.js
@@ -1,6 +1,1 @@
-/**
- * Created by alinaoros on 11/16/2015.
- */
-var React = require('react');
 
-var addCategory
