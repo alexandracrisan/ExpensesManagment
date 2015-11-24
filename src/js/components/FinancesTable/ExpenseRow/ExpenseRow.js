07f395abc0cf476cb791d1c90d1e30e5547ee6da
Diana Plesa <diana.plesa@evozon.com>
2015-11-24 09:45:52 +0200

    added functionality for api calls for login

---
+++ src/js/components/FinancesTable/ExpenseRow/ExpenseRow.js
@@ -0,0 +1,91 @@
+/**
+ * Created by crisanalexandra on 11/16/2015.
+ */
+
+var React = require('react'),
+    FinanceStore = require('../../../stores/finances-store.js'),
+    CheckBox = require('../../Common/checkBox.js'),
+    InlineEdit = require('react-inline-edit');
+
+
+var ExpenseRow = React.createClass({
+
+
+    getInitialState: function() {
+      return {
+          expense: this.props.expense,
+          editing: true
+      }
+    },
+
+    //componentWillMount: function(){
+    //    FinanceStore.addChangeListener(this._onChange)
+    //},
+    //
+    //componentWillUnmount: function(){
+    //    FinanceStore.removeChangeListener(this._onChange);
+    //},
+
+    //_onChange: function(){
+    //    this.setState(this.getExpenseList());
+    //},
+
+    determinateValueCredit: function() {
+        var currentExpense = this.state.expense;
+        if(currentExpense.amount < 0) {
+            return (<td contentEditable="true">{currentExpense.amount}</td>);
+        }
+        else {
+            return (<td contentEditable="false">-</td>);
+        }
+    },
+
+    determinateValueDebit: function() {
+        var currentExpense = this.state.expense;
+        if(currentExpense.amount > 0) {
+            return (<td contentEditable="true">{currentExpense.amount}</td>);
+        }
+        else {
+            return (<td contentEditable="false">-</td>);
+        }
+    },
+
+    onSave: function() {
+        this.replaceState(this.getInitialState())
+    },
+
+    onCancel: function() {
+        this.replaceState(this.getInitialState())
+    },
+
+    onChange: function(param) {
+        this.setState({expense: param});
+    },
+
+    render: function() {
+        var currentExpense = this.state.expense;
+        {console.log(currentExpense)}
+        return (
+
+            <tr id={currentExpense.id}>
+                <td contentEditable="false">{currentExpense.id}</td>
+                <td contentEditable="true">{currentExpense.title}</td>
+                <td contentEditable="true">{currentExpense.description}</td>
+
+                {this.determinateValueCredit()}
+                {this.determinateValueDebit()}
+
+                <td contentEditable="true">{currentExpense.from}</td>
+                <td contentEditable="true">{currentExpense.to}</td>
+                <td contentEditable="true">{currentExpense.created}</td>
+                <td contentEditable="true">{currentExpense.updated}</td>
+                <td contentEditable="true">{currentExpense.typeid}</td>
+                <td contentEditable="true">{currentExpense.tags}</td>
+                <td><CheckBox /></td>
+            </tr>
+
+        );
+    }
+});
+
+module.exports = ExpenseRow;
--- src/js/stores/finances-store.js
+++ src/js/stores/finances-store.js
@@ -17,8 +17,7 @@ function _addMovementItem(finance){
 
 function _addCategoryItem(finance){
 
-  var data = ApiCalls.categories.add(finance);
-
+  var data = ApiCalls._categories.add(finance);
 }
 
 var FinanceStore = assign({}, EventEmitter.prototype, {
@@ -30,13 +29,13 @@ var FinanceStore = assign({}, EventEmitter.prototype, {
     refreshData: function() {
        
        ApiCalls.movements.get(function(response){
-            if(response.result){
-                _movements = response.data;
-                console.log(_movements);
-                FinanceStore.emitChange();
-            }
-            else {console.log(response);}
-        });
+        		if(response.result){
+        			_movements = response.data;
+        			console.log(_movements);
+        			FinanceStore.emitChange();
+        		}
+        		else {console.log(response);}
+        	});        
     },
 
     getData: function(){
@@ -47,29 +46,15 @@ var FinanceStore = assign({}, EventEmitter.prototype, {
     addMovement: function(movement){
 
     	ApiCalls.movements.add(movement, function(response){
-            if(response.result){
-
-                FinanceStore.refreshData();
-            }
-            else {
-                console.log(response);
-            }
-        });
+        		if(response.result){
+        			
+        			FinanceStore.refreshData();
+        		}
+        		else {
+        			console.log(response);
+        		}
+        	});
     },
-
-    editMovement: function(movement){
-
-        ApiCalls.movements.update(movement, function(response){
-            if(response.result){
-
-                FinanceStore.refreshData();
-            }
-            else {
-                console.log(response);
-            }
-        });
-    },
-
 
     addChangeListener: function(callback) {
         this.on(CHANGE_EVENT, callback);
@@ -84,13 +69,11 @@ var FinanceStore = assign({}, EventEmitter.prototype, {
 FinanceStore.dispatchToken = AppDispatcher.register(function(action){
 
     switch(action.type){
-        case FinanceConstants.ActionTypes.ADD_FINANCE:
+       case FinanceConstants.ActionTypes.ADD_FINANCE:
            FinanceStore.addMovement(action.data);
+           
            break;
-        case FinanceConstants.ActionTypes.EDIT_FINANCE:
-            FinanceStore.editMovement(action.data);
-            break;
-        default:
+       default:
     }
 
 });
